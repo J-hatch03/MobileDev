45dc2697a787170d62f4d9b16d427a0961b1100a
Jaren Hatch <A02236841@aggies.usu.edu>
2026-02-08 13:03:18 -0700

    my last try to commit and pushassignment 2

---
+++ realAssignment2/components/TaskList.jsx
@@ -0,0 +1,8 @@
+export const myList = [
+    {id: 1, text: "Apples", isChecked: false },
+    {id: 2, text: "Milk", isChecked: false },
+    {id: 3, text: "Cereal", isChecked: true },
+    {id: 4, text: "Yogurt", isChecked: false },
+];
+
+ 
