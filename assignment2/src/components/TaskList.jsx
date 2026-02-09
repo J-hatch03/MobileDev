3e4eae8df78b41dc348f64ec063aa8aae0e84724
Jaren Hatch <A02236841@aggies.usu.edu>
2026-02-09 16:43:01 -0700

    delete assignment 2

--- assignment2/src/components/TaskList.jsx
+++
@@ -1,8 +0,0 @@
-export const myList = [
-    {id: 1, text: "Apples", isChecked: false },
-    {id: 2, text: "Milk", isChecked: false },
-    {id: 3, text: "Cereal", isChecked: true },
-    {id: 4, text: "Yogurt", isChecked: false },
-];
-
- 
--- assignment2/src/components/header.jsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  const myHeader = "My Tasks";
-
-  return (
-    <header>
-      <h1>{myHeader}</h1>
-    </header>
-  );
-}
