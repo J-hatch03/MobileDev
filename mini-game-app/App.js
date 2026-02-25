6a3006d78455ab788c52156e892695e43a4d1b65
Jaren Hatch <A02236841@aggies.usu.edu>
2026-02-25 16:27:15 -0700

    added gradient and background image

--- mini-game-app/App.js
+++ mini-game-app/App.js
@@ -1,18 +1,28 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 
 export default function App() {
   return (
-  <View style={styles.rootScreen}>
-    <StartGameScreen />
-  </View>
+  <LinearGradient colors={['#4e0324', '#ddb52f']} style={styles.rootScreen}>
+    <ImageBackground 
+      source={require('./assets/splash-icon.png')} 
+      resizeMode="cover"
+      style={styles.rootScreen}
+      imageStyle={styles.backgroundImage}>
+      <StartGameScreen />
+    </ImageBackground>
+    
+  </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: '#ddb52f',
-  }
+  },
+backgroundImage: {
+  opacity: 0.15,
+}
 });
