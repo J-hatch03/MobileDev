bed8f76628e645c0ee1d83fe2191c3f74fa8c23f
Jaren Hatch <A02236841@aggies.usu.edu>
2026-02-24 20:53:07 -0700

    got to video linear gradient. couldnt get expo linear gradient to work

--- mini-game-app/App.js
+++ mini-game-app/App.js
@@ -3,10 +3,16 @@ import StartGameScreen from "./screens/StartGameScreen";
 
 
 export default function App() {
-  return <StartGameScreen />
-
+  return (
+  <View style={styles.rootScreen}>
+    <StartGameScreen />
+  </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  
+  rootScreen: {
+    flex: 1,
+    backgroundColor: '#ddb52f',
+  }
 });
